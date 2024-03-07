40f6d51795664128e4069eff54039f3d5870bd3a
Jai Paragas <iaj.paragas@yahoo.com>
2024-03-07 21:10:08 +0800

    Refactir

--- src/Api/Services/DataProvider.cs
+++ src/Api/Services/DataProvider.cs
@@ -1,4 +1,6 @@
 using Binance.Net.Clients;
+using Binance.Net.Enums;
+using Binance.Net.Objects.Models.Spot;
 using Skender.Stock.Indicators;
 
 namespace Api.Services;
@@ -14,53 +16,47 @@ public class DataProvider
     {
         DateTime currentDate = DateTime.UtcNow.Date;
         var tasks = new List<Task<Quote?>>();
+
         for (int i = 0; i < (YEAR_COUNT * 365); i++)
         {
             DateTime date = currentDate.AddDays(-i);
 
             foreach (string ticker in watchList)
             {
-                var task = Task.Run(async () =>
-                {
-                    var candlestickData = await binanceClient.SpotApi.ExchangeData.GetKlinesAsync(
-                        ticker,
-                        Binance.Net.Enums.KlineInterval.OneDay,
-                        startTime: date,
-                        endTime: date
-                    );
-
-                    Guard.Against.Null(candlestickData, nameof(candlestickData), "Cannot be null.");
-
-                    if (!candlestickData.Data.Result.Any())
-                    {
-                        return null;
-                    }
-
-                    decimal openPrice = candlestickData.Data.Result.First().OpenPrice;
-                    decimal highPrice = candlestickData.Data.Result.First().HighPrice;
-                    decimal lowPrice = candlestickData.Data.Result.First().LowPrice;
-                    decimal closePrice = candlestickData.Data.Result.First().ClosePrice;
-                    decimal volume = candlestickData.Data.Result.First().Volume;
-
-                    return new Quote()
-                    {
-                        Date = date,
-                        Open = openPrice,
-                        High = highPrice,
-                        Low = lowPrice,
-                        Close = closePrice,
-                        Volume = volume
-                    };
-                });
-
-                if (task is not null)
-                {
-                    tasks.Add(task);
-                }
+                var task = FetchQuoteAsync(binanceClient, ticker, date);
+                tasks.Add(task);
             }
         }
 
-        var quotes = await Task.WhenAll(tasks);
+        var quotes = (await Task.WhenAll(tasks)).Where(x => x != null);
         return quotes!;
+    }
+
+    private static async Task<Quote?> FetchQuoteAsync(
+        BinanceSocketClient binanceClient,
+        string ticker,
+        DateTime date
+    )
+    {
+        var candle = await binanceClient.SpotApi.ExchangeData.GetKlinesAsync(
+            ticker,
+            KlineInterval.OneDay,
+            startTime: date,
+            endTime: date
+        );
+
+        Guard.Against.Null(candle, nameof(candle), "Cannot be null.");
+
+        return candle.Data.Result.FirstOrDefault() is BinanceSpotKline result
+            ? new Quote()
+            {
+                Date = date,
+                Open = result.OpenPrice,
+                High = result.HighPrice,
+                Low = result.LowPrice,
+                Close = result.ClosePrice,
+                Volume = result.Volume
+            }
+            : null;
     }
 }
